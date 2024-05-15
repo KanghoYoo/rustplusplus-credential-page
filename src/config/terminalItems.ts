202e68412d3ca16cf9bee1d58c798fc9f2e393c3
KanghoYoo <kangsick98@gmail.com>
2024-05-16 01:31:52 +0900

    add: Data for code content in Terminal component

---
+++ src/config/terminalItems.ts
@@ -0,0 +1,10 @@
+export const terminalItems = [
+  {
+    id: 'clone-the-repository',
+    code: [
+      { id: 'clone-repo', line: '$ git clone https://github.com/alexemanuelol/rustplusplus.git' },
+      { id: 'change-directory', line: '$ cd rustplusplus' },
+      { id: 'install-dependencies', line: '$ npm install' },
+    ],
+  },
+];
